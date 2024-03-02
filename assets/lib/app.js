b1d492bbf3f4be40c6501ccee6b11ef3d3bc6546
mzaini30 <muhzaini30@gmail.com>
2024-03-03 04:51:18 +0800

    feat: nambah tulisan di jangan batasi dirimu

--- assets/lib/app.js
+++ assets/lib/app.js
@@ -1,1 +1,24 @@
 document.querySelector('html').dataset.theme = 'light';
+
+var index = [
+  {
+    judul: 'Jangan Batasi Dirimu',
+    file: 'jangan-batasi-dirimu'
+  }
+]
+
+setTimeout(function () {
+  var textarea = document.querySelector('textarea');
+  if (textarea) {
+    textarea.parentNode.querySelector('section').outerHTML = /*html*/ `
+    <div class='container'>
+      <article>
+        ${marked.parse(textarea.value)}
+      </article>
+    </div>
+    `
+
+    // marked.parse(textarea.value);
+
+  }
+})
